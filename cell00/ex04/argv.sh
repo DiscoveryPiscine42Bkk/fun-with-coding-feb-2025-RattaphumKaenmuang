666c73f8369f84dfe3bd6a855db34f0906e4f009
Rattaphum Kaenmuang <66010704@KMITL.AC.TH>
2025-02-04 22:35:18 +0700

    added edge case in c0.4

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -3,6 +3,10 @@
 if [ $# -gt 0 ]; then
 	for arg in "$@"; do
 		echo "$arg"
+		count=$((count + 1))
+        if [ $count -ge 3 ]; then
+            break
+        fi
 	done
 else
 	echo "No arguments supplied"
