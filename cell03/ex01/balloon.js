1721bf19a671f01ecd3ad1a1e16fb540c02acffd
Rattaphum Kaenmuang <66010704@KMITL.AC.TH>
2025-02-06 09:44:04 +0700

    c3.1: color changes in reverse on mouseleave

--- cell03/ex01/balloon.js
+++ cell03/ex01/balloon.js
@@ -36,9 +36,18 @@ function handleLeave() {
     const balloon = document.getElementById('balloon');
     currentWidth = window.getComputedStyle(balloon).width;
     currentHeight = window.getComputedStyle(balloon).height;
+    currentColor = window.getComputedStyle(balloon).backgroundColor;
 
     if (currentWidth > '200px') {
         balloon.style.width = parseFloat(currentWidth) - 10 + 'px';
         balloon.style.height = parseFloat(currentHeight) - 10 + 'px';
+    }
+
+    if (currentColor === 'rgb(255, 0, 0)') {
+        balloon.style.backgroundColor = 'rgb(0, 0, 255)';
+    } else if (currentColor === 'rgb(0, 0, 255)') {
+        balloon.style.backgroundColor = 'rgb(0, 255, 0)';
+    } else {
+        balloon.style.backgroundColor = 'rgb(255, 0, 0)';
     }
 }
